8adce0775db5a87a69160d577261920215750d23
Dylan Wayne Foster <dylanwaynefoster@gmail.com>
2017-01-04 18:40:36 -0500

    Added beer mug icon to Frame

--- src/com/aleculator/aframe/AleFrame.java
+++ src/com/aleculator/aframe/AleFrame.java
@@ -4,12 +4,14 @@ package com.aleculator.aframe;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.Color;
 
 @SuppressWarnings("serial")
 public class AleFrame extends JFrame {
 	
 	final int WIDTH = 550;
 	final int HEIGHT = 550;
+	ImageIcon mug = new ImageIcon("beerMug.png");
 	
 	public AleFrame()
 	
@@ -23,6 +25,7 @@ public class AleFrame extends JFrame {
 		myMessage.setFont(new Font("Courier New", Font.ITALIC, 16));
 		myMessage.setBackground(Color.BLACK);
 		myMessage.setOpaque(true);
+		setIconImage(mug.getImage());
 		add(myMessage);
 		
 	}
